ca8c9d4fd1333ecca11d6253c5806c57cb3ec702
S-Kiev <vezequiel58@gmail.com>
2024-03-08 15:34:11 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express().use(bodyParser.json());
+
+app.post('/webhook', (req, res) => {
+
+    const body = req.body;
+
+    if(body.object === 'page') {
+        body.entry.forEach((entry) => {
+            const webhookEvent = entry.messaging[0];
+            console.log(webhookEvent);
+        });
+
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+app.get('/webhook', (req, res) => {
+    const verify_token = 'dsdsdsfwrfrwfwrfwrfrwfrw'
+
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+
+    if(mode && token) {
+        if(mode === 'subscribe' && token === verify_token) {
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+        } else {
+            res.sendStatus(403);
+        }
+    }
+    res.send('GET request received');
+});
+
+
+app.listen(PORT, () => {
+    console.log('Servidor iniciado');
+});
